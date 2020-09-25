bfe4147290fe968775c1d73b825027ebebc9e78f
laijihua <1164258202@qq.com>
2020-09-25 08:49:59 +0800

    初始化

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("the secret number is : {}", secret_number);
+    println!("please input your guess");
+
+    
+
+    
+
+    loop {
+
+        println!("please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Your guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    }
+
+    
+}
